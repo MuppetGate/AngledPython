0da6694ed6c0447a63deca0d98561245719c969d
Ray Offiah <ray.offiah@appsbroker.com>
2013-09-25 11:12:03 +0100

    Added more stuff for angular showing and hiding.

--- Poll/static/js/myTutorialController.js
+++ Poll/static/js/myTutorialController.js
@@ -5,5 +5,54 @@
 app.controller("MyTutorialController", function($scope){
 
     $scope.greetings = "Hello world!"
+    $scope.inputValue = ""
+
+    $scope.selectedPerson = 0;
+    $scope.selectedGenre = null;
+    $scope.people = [
+        {
+            id: 0,
+            name: 'Leon',
+            music: [
+                'Rock',
+                'Metal',
+                'Dubstep',
+                'Electro'
+            ],
+            live: true
+        },
+        {
+            id: 1,
+            name: 'Chris',
+            music: [
+                'Indie',
+                'Drumstep',
+                'Dubstep',
+                'Electro'
+            ],
+            live: false
+        },
+        {
+            id: 2,
+            name: 'Harry',
+            music: [
+                'Rock',
+                'Metal',
+                'Thrash Metal',
+                'Heavy Metal'
+            ],
+            live: false
+        },
+        {
+            id: 3,
+            name: 'Allyce',
+            music: [
+                'Pop',
+                'RnB',
+                'Hip Hop'
+            ],
+            live: true
+        }
+    ];
 
 });
